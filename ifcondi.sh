220f4d471047515af9d4ba80791a382c8f14c847
gunnys2303 <gunnys2303@gmail.com>
2024-05-03 00:16:43 +0530

    update

---
+++ ifcondi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter command :"
+read command
+a=`$command`
+if [[ "$?" == 0 ]];
+then
+echo "command successful,Great"
+else
+echo "command failed to run ,verify once again"
+fi
